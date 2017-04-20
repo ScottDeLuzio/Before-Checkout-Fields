fc0c0758d00cc294cfbeba988f7af300c98836cf
Scott DeLuzio <ScottDeLuzio@users.noreply.github.com>
2017-04-20 11:47:20 -0700

    Add files via upload

--- cwcfp-before-checkout-fields.php
+++ cwcfp-before-checkout-fields.php
@@ -3,7 +3,7 @@
    Plugin Name: Before Checkout Fields
    Plugin URI: https://conditionalcheckoutfields.com
    Description: Display content before the checkout fields
-   Version: 1.0.0
+   Version: 1.0.1
    Author: Scott DeLuzio
    Author URI: https://scottdeluzio.com
    */
@@ -18,7 +18,28 @@ function show_children_information(){
 }
 add_action( 'cwcfp_before_checkout_fields', 'show_children_information', 10 );
 
-function show_children_group( $repeat ){
-   echo '<strong>Child ' . $repeat . ':</strong>';
+function show_children_group( $repeat, $field_id ){
+   switch ( $field_id ) {
+      case '1':
+         /* This would be the English version that would show up for field #1
+          * Add case 'XX': etc. under case '1': above to add additional fields.
+          * Replace '1' or 'XX' with actual field IDs.
+          */
+         echo '<strong>Child ' . $repeat . ':</strong>';
+         break;
+      case '2':
+         /* This would be the Spanish version that would show up for field #3
+          * Add case 'XX': etc. under case '2': above to add additional fields.
+          * Replace '1' or 'XX' with actual field IDs.
+          */
+         echo '<strong>Niño ' . $repeat . ':</strong>';
+         break;
+      
+      default:
+         /* You can add a default message here, or leave blank if you only want
+          * this to display for a handful of fields.
+          */
+         break;
+   }
 }
-add_action( 'cwcfp_before_checkout_fields_grouping', 'show_children_group', 10, 1 );
+add_action( 'cwcfp_before_checkout_fields_grouping', 'show_children_group', 10, 2 );
